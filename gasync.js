96a44b8555aee46b0ddff5afcaa9f20a879ff90e
vince19972 <vince19972@gmail.com>
2020-03-30 15:39:37 -0400

    [promise] send message to discord by callback fn

--- gasync.js
+++ gasync.js
@@ -1,41 +1,38 @@
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
+const fs = require("fs");
+const readline = require("readline");
+const { google } = require("googleapis");
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = 'token.json';
+const TOKEN_PATH = "token.json";
 
 // Load client secrets from a local file.
-function fetchGoogle() {
+function fetchGoogle(bot) {
   // return new Promise((resolve, reject) => {
-    // readCred();
-    return new Promise((resolve, reject) => {
-      fs.readFile('credentials.json', (err, content) => {
-          if (err) return console.log('Error loading client secret file:', err);
-            // Authorize a client with credentials, then call the Google Sheets API.
-            authorize(JSON.parse(content), datapull);
-      });
-    })
-}
-
-function readCred(){
-  let prom = new Promise((resolve, reject) => {
-    fs.readFile('credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
+  // readCred();
+  new Promise((resolve, reject) => {
+    fs.readFile("credentials.json", (err, content) => {
+      if (err) return console.log("Error loading client secret file:", err);
       // Authorize a client with credentials, then call the Google Sheets API.
-    // authorize(JSON.parse(content), datapull);
-        resolve(content)
-      })
-    })
-    prom.then((credentials)=>
-      authorize(JSON.parse(credentials), datapull)
-    )
+      authorize(JSON.parse(content), datapull, bot);
+    });
+  });
 }
 
+function readCred() {
+  let prom = new Promise((resolve, reject) => {
+    fs.readFile("credentials.json", (err, content) => {
+      if (err) return console.log("Error loading client secret file:", err);
+      // Authorize a client with credentials, then call the Google Sheets API.
+      // authorize(JSON.parse(content), datapull);
+      resolve(content);
+    });
+  });
+  prom.then(credentials => authorize(JSON.parse(credentials), datapull));
+}
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -43,16 +40,22 @@ function readCred(){
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
+function authorize(credentials, callback, bot) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
     if (err) return getNewToken(oAuth2Client, callback);
+
     oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
+
+    // console.log(callback(oAuth2Client), "testing");
+    callback(oAuth2Client, bot);
   });
 }
 
@@ -64,23 +67,27 @@ function authorize(credentials, callback) {
  */
 function getNewToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
+    access_type: "offline",
+    scope: SCOPES
   });
-  console.log('Authorize this app by visiting this url:', authUrl);
+  console.log("Authorize this app by visiting this url:", authUrl);
   const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout,
+    output: process.stdout
   });
-  rl.question('Enter the code from that page here: ', (code) => {
+  rl.question("Enter the code from that page here: ", code => {
     rl.close();
     oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error while trying to retrieve access token', err);
+      if (err)
+        return console.error(
+          "Error while trying to retrieve access token",
+          err
+        );
       oAuth2Client.setCredentials(token);
       // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
         if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
+        console.log("Token stored to", TOKEN_PATH);
       });
       callback(oAuth2Client);
     });
@@ -92,26 +99,44 @@ function getNewToken(oAuth2Client, callback) {
  * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
-function datapull(auth) {
-  const sheets = google.sheets({version: 'v4', auth});
+function datapull(auth, bot) {
+  const sheets = google.sheets({ version: "v4", auth });
 
-    sheets.spreadsheets.values.get({
-      spreadsheetId:  '1cg9E0APQNpHEHJ3nklFEv_RFF2-h51qdXEe-6h55FiQ',
-      range: 'Sheet1!A2:E',
+  // return new Promise((resolve, reject) => {
+  sheets.spreadsheets.values
+    .get({
+      spreadsheetId: "1cg9E0APQNpHEHJ3nklFEv_RFF2-h51qdXEe-6h55FiQ",
+      range: "Sheet1!A2:E"
     })
-    .then((res)=> {
+    .then(res => {
       const rows = res.data.values;
-      console.log(rows)
-      return res.data.values
-    })
-    .catch(err=>console.error(err));
 
-    //   if (err) return console.log('The API returned an error: ' + err);
-    //   const rows = res.data.values;
-    //   console.log(rows)
-    //   return rows
-    // });
-    // return result
+      res.data.values.forEach(res => {
+        bot.sendMsg(res);
+      });
+    })
+    .catch(err => console.error(err));
+  // });
+
+  // sheets.spreadsheets.values
+  //   .get({
+  //     spreadsheetId: "1cg9E0APQNpHEHJ3nklFEv_RFF2-h51qdXEe-6h55FiQ",
+  //     range: "Sheet1!A2:E"
+  //   })
+  //   .then(res => {
+  //     const rows = res.data.values;
+  //     // console.log(rows);
+  //     // return res.data.values;
+  //     return rows;
+  //   })
+  //   .catch(err => console.error(err));
+
+  //   if (err) return console.log('The API returned an error: ' + err);
+  //   const rows = res.data.values;
+  //   console.log(rows)
+  //   return rows
+  // });
+  // return result
 }
 
 exports.fetchGoogle = fetchGoogle;
