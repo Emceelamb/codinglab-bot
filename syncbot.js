059ac1ac205a14d908caf2e5bd7964cf9367314f
vince19972 <vince19972@gmail.com>
2020-03-30 19:11:13 -0400

    [syncbot] restore useful code…

--- syncbot.js
+++ syncbot.js
@@ -30,6 +30,13 @@ bot.on("message", function(user, userID, channelID, message, evt) {
     var args = message.substring(1).split(" ");
     var cmd = args[0];
 
+    function sendMsg(res) {
+      bot.sendMessage({
+        to: channelID,
+        message: res
+      });
+    }
+
     args = args.splice(1);
     switch (cmd) {
       // !ping
