a69d1367205564c2cf5ae6512a86bf925af83ac9
billythemusical <bennett.billy@gmail.com>
2020-03-31 22:10:23 -0400

    Changed the match messages.

--- botMsgActions.js
+++ botMsgActions.js
@@ -11,13 +11,13 @@ function botMsgActions(bot, channelID) {
     sendAll(data) {
 
       let counselorIndex = 0;
-      let counselorTable = "```\nCounselor \t | \t Time \t | Zoom ID  "
+      let counselorTable = "```\nCounselor \t | \t Time \t | Zoom Mtg ID  "
       counselorTable += "\rSkills\n"
       counselorTable += "----------------\n"
 
       data.forEach((counselorInfo, counselorIndex, data) => {
         let zoomId=counselorInfo[3].slice(-10)
-        counselorTable+=`${counselorInfo[0]} | ${counselorInfo[2]} | Zoom ID: ${zoomId}\n${counselorInfo[1]}  \n\n`
+        counselorTable+=`${counselorInfo[0]} | ${counselorInfo[2]} | Zoom Mtg ID: ${zoomId}\n${counselorInfo[1]}  \n\n`
           counselorIndex++;
           if(counselorIndex === data.length){
             counselorTable+='```'
@@ -36,14 +36,26 @@ function botMsgActions(bot, channelID) {
         );
       });
 
+      let matchedIndex = 0;
+      let msg = '```';
+
+      // Would prefer this format
+      // Billy knows p5.js - try Monday 2-5pm!
+      // Mark knows p5.js - try Tuesday 2-5pm!
+
       if (matched.length > 0) {
-        matched.forEach(counselor => {
-          sendMsg(
-            `${counselor[0]} knows ${keyword}! Book the office hour at ${counselor[2]}`
-          );
+        matched.forEach((counselor, matchedIndex, matched) => {
+          msg += `${counselor[0]} knows ${keyword}! Try ${counselor[2]}\n`;
+          matchedIndex++;
+          if(matchedIndex === matched.length){
+            msg+='```'
+            sendMsg(`
+              ${msg}
+              `);
+          }
         });
       } else {
-        sendMsg(`😅 Sorry! No one knows ${keyword} at this moment.`);
+        sendMsg(`😅 Sorry! We have no ${keyword} experts at this time.  Try an ITP resident or faculty member here: https://itp.nyu.edu/help/in-person-help/office-hours/`);
       }
     }
   };
