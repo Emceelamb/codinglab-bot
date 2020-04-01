4bb9cf7ef6a8c7483310d7ea5f1205035318441d
vince19972 <vince19972@gmail.com>
2020-04-01 15:57:15 -0400

    [bot] clean up and comment the code

--- botMsgActions.js
+++ botMsgActions.js
@@ -1,35 +1,64 @@
-/**
- * Getting keyword in database that's synonymous with input:
- * @param {string} input
- */
-function getSynonymous(input) {
-  switch (input) {
-    case "js":
-      return "javascript";
-    case "react":
-      return "reactjs";
-    case "vue":
-      return "vuejs";
-    case "p5":
-      return "p5js";
-    case "node":
-      return "nodejs";
-    default:
-      return input;
-  }
-}
+const getter = {
+  /**
+   * Getting keyword in database that's synonymous with input:
+   * @param {string} input
+   */
+  synonymous(input) {
+    switch (input) {
+      case "js":
+        return "javascript";
+      case "react":
+        return "reactjs";
+      case "vue":
+        return "vuejs";
+      case "p5":
+        return "p5js";
+      case "node":
+        return "nodejs";
+      case "max" || "msp" || "jitter":
+        return "maxmsp";
+      default:
+        return input;
+    }
+  },
 
-/**
- * Format keyword into shape that conforms database skill strings:
- * @param {string} keyword
- */
-function getFormattedKeyword(keyword) {
-  const regexPattern = /\w/gi;
-  return keyword
-    .match(regexPattern)
-    .join("")
-    .toLowerCase();
-}
+  /**
+   * Get counselors that have skills matched with input keyword:
+   * @param {object} data
+   */
+
+  skillMatchedCounselors(data, inputSkill) {
+    return data.filter(counselorInfo => {
+      // formatted skills is matched with F column in the sheet
+      const rawSkills = counselorInfo[5].split(", ");
+      const formattedInputSkill = getter.synonymous(
+        setter.formatKeyword(inputSkill)
+      );
+
+      return (
+        rawSkills.filter(skill => {
+          const formattedSkill = setter.formatKeyword(skill);
+          return formattedSkill === formattedInputSkill;
+        }).length > 0
+      );
+      c;
+    });
+  }
+};
+
+const setter = {
+  /**
+   * Format keyword into shape that conforms database skill strings:
+   * @param {string} keyword
+   */
+  formatKeyword(keyword) {
+    const regexPattern = /\w/gi;
+    return keyword
+      .match(regexPattern)
+      .join("")
+      .toLowerCase();
+  }
+};
 
 /**
  * Function that includes message sending actions of the bot:
@@ -46,17 +75,17 @@ function botMsgActions(bot, channelID) {
 
   return {
     sendAll(data) {
-      let counselorIndex = 0;
-      // let counselorTable = "```\nCounselor \t | \t Time \t | Zoom ID"
-      // counselorTable += "\rSkills\n"
       let counselorTable = "```*** Coding Lab Tech Info *** \n\n";
-      // counselorTable += "----------------\n"
 
-      data.forEach((counselorInfo, counselorIndex, data) => {
+      // 1. loop through fetched array of data
+      data.forEach((counselorInfo, counselorIndex) => {
+        // 2. Initiate start of the message sending to Discord
         let zoomId = counselorInfo[3].slice(-10);
         counselorTable += `${counselorInfo[0]} | ${counselorInfo[2]} | ZoomID: ${zoomId}\n${counselorInfo[1]}  \n\n`;
-        counselorIndex++;
-        if (counselorIndex === data.length) {
+
+        // 3. Send the message at the end of the array
+        const isLastCounselor = counselorIndex === data.length - 1;
+        if (isLastCounselor) {
           counselorTable += "```";
           sendMsg(`
               ${counselorTable}
@@ -65,30 +94,16 @@ function botMsgActions(bot, channelID) {
       });
     },
     sendSkillMatched(data, inputSkill) {
-      const matched = data.filter(counselorInfo => {
-        // formatted skills is matched with F column in the sheet
-        const rawSkills = counselorInfo[5].split(", ");
-        const formattedInputSkill = getSynonymous(
-          getFormattedKeyword(inputSkill)
-        );
+      const matchedOnes = getter.skillMatchedCounselors(data, inputSkill);
 
-        return (
-          rawSkills.filter(skill => {
-            const formattedSkill = getFormattedKeyword(skill);
-            return formattedSkill === formattedInputSkill;
-          }).length > 0
-        );
-        c;
-      });
-
-      if (matched.length > 0) {
-        let matchedIndex = 0;
+      if (matchedOnes.length > 0) {
         let msg = "```";
 
-        matched.forEach(counselor => {
+        matchedOnes.forEach((counselor, counselorIndex) => {
           msg += `${counselor[0]} knows ${inputSkill}! Try ${counselor[2]}\n`;
-          matchedIndex++;
-          if (matchedIndex === matched.length) {
+
+          const isLastCounselor = counselorIndex === matchedOnes.length - 1;
+          if (isLastCounselor) {
             msg += "```";
             msg +=
               "Book hours here: https://calendar.google.com/calendar/selfsched?sstoken=UUQtTkNDbVFiUEhRfGRlZmF1bHR8NGNkOWNlZWVjOTZhYzI0MjAxNDYyMzFiMTJmNWZiZmE";
